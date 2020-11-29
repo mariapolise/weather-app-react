6804d94d7fcc7e3f022d9a030b6d5bbce0dc545a
Maria Polise <70155477+mariapolise@users.noreply.github.com>
2020-11-29 14:32:38 -0500

    Changed Greeting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Again</h1>
+        <h1>Hello Again...</h1>
         <Test />
       </header>
     </div>
