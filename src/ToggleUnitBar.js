757df5b4bc8d1da40eb1c00882e76e1a30d54f44
Maria Polise <70155477+mariapolise@users.noreply.github.com>
2020-12-09 19:11:04 -0500

    CSS toggle unit bar

--- src/ToggleUnitBar.js
+++ src/ToggleUnitBar.js
@@ -3,7 +3,7 @@ import "./ToggleUnitBar.css";
 
 export default function ToggleUnitBar() {
   return (
-    <div className="col col-3 toggle-unit toggle-unit-bar">
+    <div className="col col-3  toggle-unit-bar">
       <button className="toggle-unit">°C</button>/
       <button className="toggle-unit">°F</button>
     </div>
